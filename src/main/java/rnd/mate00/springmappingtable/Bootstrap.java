6e2773ab1972dd512852d964a275925e714a1d18
mate0021 <mami.ext@gmail.com>
2022-06-10 12:47:43 +0200

    Added more countries.

--- src/main/java/rnd/mate00/springmappingtable/Bootstrap.java
+++ src/main/java/rnd/mate00/springmappingtable/Bootstrap.java
@@ -5,8 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import rnd.mate00.springmappingtable.entity.Country;
 import rnd.mate00.springmappingtable.entity.Office;
+import rnd.mate00.springmappingtable.entity.Product;
 import rnd.mate00.springmappingtable.entity.Region;
 import rnd.mate00.springmappingtable.repository.*;
+
+import java.util.List;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
@@ -26,9 +29,25 @@ public class Bootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        addOffices();
-        addCountries();
         addRegions();
+        addCountries();
+        addOffices();
+
+        Product prod1 = new Product("my product 1");
+        prod1.setCountries(List.of(
+                countryRepository.findByCountryName("Germany").orElse(null)
+        ));
+        prod1.setOffices(List.of(
+                officeRepository.findByOfficeName("pol1").orElse(null),
+                officeRepository.findByOfficeName("pol2").orElse(null)
+        ));
+        productRepository.save(prod1);
+
+        Product prod2 = new Product("product 2");
+        prod2.setRegions(List.of(
+                regionRepository.findByRegionName("EMEA").orElse(null)
+        ));
+        productRepository.save(prod2);
     }
 
     private void addOffices() {
@@ -38,18 +57,30 @@ public class Bootstrap implements CommandLineRunner {
 
         Office ger1 = new Office("ger1");
         Office ger2 = new Office("ger2");
+
+        Office fra1 = new Office("fra1");
+        Office fra2 = new Office("fra2");
+        Office fra3 = new Office("fra3");
+
         officeRepository.save(pol1);
         officeRepository.save(pol2);
         officeRepository.save(pol3);
         officeRepository.save(ger1);
         officeRepository.save(ger2);
+        officeRepository.save(fra1);
+        officeRepository.save(fra2);
+        officeRepository.save(fra3);
     }
 
     private void addCountries() {
         Country pol = new Country("Poland");
         Country ger = new Country("Germany");
+        Country fra = new Country("France");
+        Country mex = new Country("Mexico");
         countryRepository.save(pol);
         countryRepository.save(ger);
+        countryRepository.save(fra);
+        countryRepository.save(mex);
     }
 
     private void addRegions() {
--- src/main/java/rnd/mate00/springmappingtable/repository/RegionRepository.java
+++ src/main/java/rnd/mate00/springmappingtable/repository/RegionRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rnd.mate00.springmappingtable.entity.Region;
 
+import java.util.Optional;
+
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Integer> {
+
+    Optional<Region> findByRegionName(String name);
 }
