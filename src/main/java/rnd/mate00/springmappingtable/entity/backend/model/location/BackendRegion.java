b44942ee026c11180d2e044dd9f673b5e9482497
Mikolajczyk Mateusz (CI/PFF3PA) <mik5lod@bosch.com>
2022-08-10 13:37:08 +0200

    Modelling location tables and relations with Product. part2

--- src/main/java/rnd/mate00/springmappingtable/entity/backend/model/location/BackendRegion.java
+++ src/main/java/rnd/mate00/springmappingtable/entity/backend/model/location/BackendRegion.java
@@ -3,11 +3,15 @@ package rnd.mate00.springmappingtable.entity.backend.model.location;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import rnd.mate00.springmappingtable.entity.backend.model.BackendProduct;
+import rnd.mate00.springmappingtable.entity.backend.model.StaticLocationRegion;
 import rnd.mate00.springmappingtable.entity.backend.model.key.ProductRegionKey;
 
-import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +24,15 @@ public class BackendRegion {
     @EmbeddedId
     private ProductRegionKey productRegionKey;
 
-    @Column
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @MapsId("productId")
+    private BackendProduct product;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    @MapsId("regionId")
+    private StaticLocationRegion region;
+
     private int jobId;
 }
