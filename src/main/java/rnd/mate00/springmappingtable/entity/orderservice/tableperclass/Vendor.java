2811173825f1158b9402b49a5bb6d39304725a20
mate0021 <mami.ext@gmail.com>
2022-08-17 12:28:29 +0200

    Table per class inheritance type.

---
+++ src/main/java/rnd/mate00/springmappingtable/entity/orderservice/tableperclass/Vendor.java
@@ -0,0 +1,37 @@
+package rnd.mate00.springmappingtable.entity.orderservice.tableperclass;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Vendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)    // <- otherwise - 'Cannot use identity column with union-subclass'
+    // also, hibernate_sequenceS table is needed
+    private int id;
+
+    private String vendorName;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+}
